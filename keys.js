c29a8acacdc28cc3465c11823b5f919b32f9d103
rfmac <rfennmacdougall@gmail.com>
2018-06-13 20:54:49 -0500

    submission3

--- keys.js
+++ keys.js
@@ -1,4 +1,7 @@
 console.log('this is loaded');
+var require = require ('./env');
+
+TWITTER_CONSUMER_KEY
 
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -6,6 +9,8 @@ exports.twitter = {
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
+
+
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
